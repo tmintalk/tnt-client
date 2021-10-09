440c881cd384ef85f8cc4aa94193161ca3e3d065
thestarkor <sk_and_mc@kaist.ac.kr>
2021-10-10 00:33:02 +0900

    [fix] 유저 태그 수정

--- src/components/UserList/index.js
+++ src/components/UserList/index.js
@@ -26,7 +26,7 @@ const UserList = () => {
             <List.Item>
               <List.Item.Meta
                 avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                title={<a href="https://ant.design">{item.nickname}</a>}
+                title={<p href="https://pnt.design">{item.nickname}</p>}
               />
             </List.Item>
           )}
