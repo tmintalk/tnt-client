2d268f269734c92e24c921a67ad7ce49cdfdca1f
thestarkor <sk_and_mc@kaist.ac.kr>
2021-10-10 15:52:54 +0900

    [fix] time 변환 에러 핸들링

--- src/services/time.js
+++ src/services/time.js
@@ -1,7 +1,11 @@
 module.exports = {
   getFormatDate: (date) => {
-    date = new Date(date);
-    date.setHours(date.getHours() + 9);
-    return date.toISOString().replace("T", " ").substring(0, 19);
+    try {
+      date = new Date(date);
+      date.setHours(date.getHours() + 9);
+      return date.toISOString().replace("T", " ").substring(0, 19);
+    } catch (error) {
+      return null;
+    }
   },
 };
