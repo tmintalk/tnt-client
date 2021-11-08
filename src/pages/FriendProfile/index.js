9f4dfcaf6f945c739119e61728dc29a9d5d0d166
woomoo00 <yumi9314@kaist.ac.kr>
2021-11-08 20:02:09 +0900

    소비패턴

--- src/pages/FriendProfile/index.js
+++ src/pages/FriendProfile/index.js
@@ -1,11 +1,13 @@
 import ProfileCard from '../../components/ProfileCard';
+import ConsumptionPattern from '../../components/ConsumptionPattern';
 
 const FriendProfile = () => {
 
   return (
     <>
-    <h3> friends page </h3>
+    {/* <h3> friends page </h3> */}
     < ProfileCard />
+    < ConsumptionPattern />
     </>
   );
 };
