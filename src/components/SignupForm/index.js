bc390139efa0c94ce8c0281974da804baa2bd427
woomoo00 <yumi9314@kaist.ac.kr>
2021-11-10 17:34:55 +0900

    [Design] 회원가입

--- src/components/SignupForm/index.js
+++ src/components/SignupForm/index.js
@@ -4,6 +4,8 @@ import { useCookies } from 'react-cookie';
 import { Form, Input, Button } from 'antd';
 import { Link } from 'react-router-dom';
 
+import './index.scss';
+
 const SignupForm = () => {
   const [, setCookie] = useCookies(['Authorization']);
 
@@ -22,38 +24,62 @@ const SignupForm = () => {
 
   return (
     <>
-      <Form
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-      >
-        <Form.Item
-          label="Nickname"
-          name="nickname"
-          rules={[{ required: true, message: '닉네임을 입력해주세요!' }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[{ required: true, message: '이메일을 입력해주세요!' }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: '비밀번호를 입력해주세요!' }]}
-        >
-          <Input.Password />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
-            회원가입
-          </Button>
-        </Form.Item>
-      </Form>
-      <Link to="/">로그인</Link>
+
+      <div className="signup-header-container"> 
+        {/* <div className='header-text'>TnT</div> */}
+        {/* <img className="header-title" src='../../commons/img/TnT.png'/> */}
+        <img src={require("../../commons/img/TnT.png").default} alt="title"/>
+      </div>
+      <div className="signup-container">
+        <div className="signup-greeting-text">
+          <div className="greeting-text-first">
+            안녕하세요! <br/> TnT 입니다.
+          </div>
+          <div className="greeting-text-second">
+            서비스 이용을 위해 회원가입이 필요해요.
+          </div>
+        </div>
+        <div className="signup-profile-photo"></div>
+        <div className="signup-form-container">
+          <Form
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+          >
+            <Form.Item
+              label="Nickname"
+              name="nickname"
+              rules={[{ required: true, message: '닉네임을 입력해주세요!' }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[{ required: true, message: '이메일을 입력해주세요!' }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[{ required: true, message: '비밀번호를 입력해주세요!' }]}
+            >
+              <Input.Password />
+            </Form.Item>
+            <div className='join-and-login'>
+              <Form.Item>
+                <Button type="primary" htmlType="submit">
+                  회원가입
+                </Button>
+              </Form.Item>
+              <Link to="/">
+                <div className="link-to-login">로그인</div>
+              </Link>
+            </div>
+          </Form>
+        </div>
+        {/* <Link to="/">로그인</Link> */}
+      </div>
     </>
   )
 }
