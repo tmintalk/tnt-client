2b22bc5466f21613da1dd1fb575f1b554a94f140
thestarkor <sk_and_mc@kaist.ac.kr>
2021-11-17 03:11:38 +0900

    [fix] mypage 좋아요

--- src/components/MyPostCard/index.js
+++ src/components/MyPostCard/index.js
@@ -1,27 +1,23 @@
 import { useState } from "react";
 
-import { useDispatch, useSelector } from "react-redux";
-import { GET_POSTS_REQUEST } from "../../reducers/posts";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
-import { IoHeart, IoChatboxOutline } from "react-icons/io5";
+import { IoHeart } from "react-icons/io5";
 import { getRoomId } from "../../actions/hash.js";
 import "./index.scss";
 
 const MyPostCard = (props) => {
-  const { posts, user } = useSelector((state) => state);
+  const { user } = useSelector((state) => state);
   const [curUser, setCurUser] = useState();
-  const dispatch = useDispatch();
 
-  const [showNameList, setShowNameList] = useState([]); // TODO: 서버랑 연결
-
-  useState(() => {
-    if (!posts.data) {
-      dispatch({
-        type: GET_POSTS_REQUEST,
-      });
-    }
-  }, [posts]);
+  // useState(() => {
+  //   if (!posts.data) {
+  //     dispatch({
+  //       type: GET_POSTS_REQUEST,
+  //     });
+  //   }
+  // }, [posts]);
 
   useState(() => {
     if (user.data) {
@@ -34,22 +30,13 @@ const MyPostCard = (props) => {
     }
   }, [user.data]);
 
-  const onClickChat = async (id) => {
-    const temp_arr = [...showNameList, id];
-    temp_arr.push(id);
-    setShowNameList(temp_arr);
-  };
-  
-
   return (
     <>
       <div className="myprofile-postcard">
-        <div className="list-post-profile"></div>
+        <img src={props?.item?.User?.thumbnailUrl} alt="thumbnail" className="list-post-profile" />
         <div className="list-post-content">
           <div className="list-post-name">
-            {showNameList.includes(props?.item?.id) || props?.isShow
-              ? props?.item?.User?.nickname
-              : props?.item?.id}
+            {props?.item?.User?.nickname}
           </div>
           <div className="sentence">
             <div className="list-post-text">spent</div>
@@ -71,14 +58,10 @@ const MyPostCard = (props) => {
 
           <div className="list-post-story">{props?.item?.description}</div>
           <div className="list-icon-container">
-            <div className="heart" onClick={() => onClickChat(props?.item?.id)}>
-              {showNameList.includes(props?.item?.id) ? (
-                <IoHeart className="heart-active" />
-              ) : (
-                <IoHeart className="heart-inactive" />
-              )}
+            <div className="heart">
+              <IoHeart className="heart-active" />
             </div>
-            <div className="liked-number"> 3k </div>
+            <div className="liked-number">{props?.item?.count}</div>
             {curUser?.name !== props?.item?.User?.nickname && (
               <Link
                 to={`/chat/${
