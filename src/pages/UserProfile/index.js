41a5db4f048d23e1521804158121033ebd20b2e8
T-dubb <rlaxodntttt@kaist.ac.kr>
2021-11-18 03:41:36 +0900

    [feat]Link to chat from user profile

--- src/pages/UserProfile/index.js
+++ src/pages/UserProfile/index.js
@@ -1,15 +1,18 @@
+import {useState } from "react";
 import UserProfileCard from "../../components/UserProfileCard";
 import ConsumptionPattern from "../../components/ConsumptionPattern";
 import UserProfileQuestionGrid from "../../components/UserProfileQuestionGrid";
+import { useSelector } from "react-redux";
 
 const FriendProfile = (props) => {
   const { id } = props.match.params;
+  const { user } = useSelector((state) => state);
   const goBack = () => {
     props.history.goBack();
   };
   return (
     <>
-      <UserProfileCard id={id} goBack={goBack} />
+      <UserProfileCard id={id} goBack={goBack} curUser={user?.data} />
       <ConsumptionPattern />
       <UserProfileQuestionGrid id={id} />
     </>
