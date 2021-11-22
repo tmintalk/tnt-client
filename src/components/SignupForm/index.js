b2fb2cf2f4e0de1fec6718f931c0023ca3c8ee4c
T-dubb <rlaxodntttt@kaist.ac.kr>
2021-11-22 19:02:13 +0900

    [feat] 회원가입 시 프로필 사진 추가 필수

--- src/components/SignupForm/index.js
+++ src/components/SignupForm/index.js
@@ -1,41 +1,43 @@
 import { useState } from "react";
-import axios from 'axios';
-import { useCookies } from 'react-cookie';
+import axios from "axios";
+import { useCookies } from "react-cookie";
 
-import { Form, Input, Button, Upload, message } from 'antd';
-import { Link } from 'react-router-dom';
-import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+import { Form, Input, Button, Upload, message } from "antd";
+import { Link } from "react-router-dom";
+import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
 
-
-import './index.scss';
+import "./index.scss";
 
 const SignupForm = () => {
-  const [, setCookie] = useCookies(['Authorization']);
+  const [, setCookie] = useCookies(["Authorization"]);
   const [loading, setLoading] = useState();
-  const [imageUrl, setImageUrl] = useState('');
+  const [imageUrl, setImageUrl] = useState("");
 
   const onFinish = async (values) => {
     const { email, password, nickname, image } = values;
     const thumbnailUrl = image?.file?.response?.uri;
 
-    const res = await axios.post('/auth/join', {
+    const res = await axios.post("/auth/join", {
       thumbnailUrl,
       email,
       password,
-      nickname
-    })
+      nickname,
+    });
 
-    setCookie('Authorization', res.data.token, { path: '/', maxAge: 24 * 60 * 60 });
-    window.location.href = "/"
-  }
+    setCookie("Authorization", res.data.token, {
+      path: "/",
+      maxAge: 24 * 60 * 60,
+    });
+    window.location.href = "/";
+  };
 
   const handleChange = (info) => {
     console.log(info);
     if (info.file.response) {
-      setImageUrl(info.file.response.uri)
-      console.log(info.file.response.uri)
+      setImageUrl(info.file.response.uri);
+      console.log(info.file.response.uri);
     }
-  }
+  };
 
   return (
     <>
@@ -49,53 +51,58 @@ const SignupForm = () => {
           </div>
           <div className="form-title">Profile Image</div>
         </div>
-        
+
         <div className="signup-form-container">
-          <Form
-            initialValues={{ remember: true }}
-            onFinish={onFinish}
-          >
+          <Form initialValues={{ remember: true }} onFinish={onFinish}>
             <Form.Item
               name="image"
+              rules={[
+                { required: true, message: "프로필 사진을 추가해주세요!" },
+              ]}
             >
               <Upload
                 name="image"
                 listType="picture-card"
                 className="avatar-uploader"
                 showUploadList={false}
-                action={'http://ec2-13-125-111-9.ap-northeast-2.compute.amazonaws.com/uploads'}
+                action={
+                  "http://ec2-13-125-111-9.ap-northeast-2.compute.amazonaws.com/uploads"
+                }
                 onChange={handleChange}
               >
-                {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%' }} /> 
-                : <div>
-                  <PlusOutlined style={{color:'#ffffff70'}}/>
-                  {/* <div style={{ marginTop: 8 , color:'#ffffff'}}>Profile</div> */}
-                </div>}
+                {imageUrl ? (
+                  <img src={imageUrl} alt="avatar" style={{ width: "100%" }} />
+                ) : (
+                  <div>
+                    <PlusOutlined style={{ color: "#ffffff70" }} />
+                    {/* <div style={{ marginTop: 8 , color:'#ffffff'}}>Profile</div> */}
+                  </div>
+                )}
               </Upload>
             </Form.Item>
 
             <Form.Item
               label="Nickname"
               name="nickname"
-              rules={[{ required: true, message: '닉네임을 입력해주세요!' }]}
+              rules={[{ required: true, message: "닉네임을 입력해주세요!" }]}
             >
               <Input />
             </Form.Item>
             <Form.Item
               label="Email"
               name="email"
-              rules={[{ required: true, message: '이메일을 입력해주세요!' }]}
+              rules={[{ required: true, message: "이메일을 입력해주세요!" }]}
             >
               <Input />
             </Form.Item>
             <Form.Item
               label="Password"
               name="password"
-              rules={[{ required: true, message: '비밀번호를 입력해주세요!' }]}
+              rules={[{ required: true, message: "비밀번호를 입력해주세요!" }]}
             >
               <Input.Password />
             </Form.Item>
-            <div className='join-and-login'>
+            <div className="join-and-login">
               <Form.Item>
                 <Button type="primary" htmlType="submit">
                   회원가입
@@ -110,7 +117,7 @@ const SignupForm = () => {
         {/* <Link to="/">로그인</Link> */}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default SignupForm
+export default SignupForm;
